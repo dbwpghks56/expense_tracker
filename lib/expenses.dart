09644b5af161656abdc4601588263c8175e252ef
wpghks <wpghks56@naver.com>
2024-07-11 15:34:29 +0900

    [Feature] expenes list widget create

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -8,14 +9,40 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Groceries',
+      amount: 50.0,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Train ticket',
+      amount: 20.0,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'Movie ticket',
+      amount: 10.0,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Laptop',
+      amount: 1000.0,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
         children: [
           Text('The chart'),
-          Text('The list of transactions'),
-        ],
+          ],
       ),
     );
   }
