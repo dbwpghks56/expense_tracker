80f2386aa341696a1e83ab03b92c6745245aaa39
JaeHwan <wpghks56@naver.com>
2024-10-15 20:50:06 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello World'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  ));
+}
