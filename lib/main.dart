f4e3c7d23a322542fb79bd0178979c2eb0657d49
JaeHwan <wpghks56@naver.com>
2024-10-16 20:22:39 +0900

    [Feature] 간단한 모델 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello World'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
     ),
-  ));
+  );
 }
